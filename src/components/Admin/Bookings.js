e1760c4279145608b7b03dadbac404779037580b
akramansari1433 <70878208+akramansari1433@users.noreply.github.com>
2021-10-23 12:44:30 +0530

    some changes

--- src/components/Admin/Bookings.js
+++ src/components/Admin/Bookings.js
@@ -3,7 +3,37 @@ import React from "react";
 function Bookings() {
    return (
       <div>
-         <h1>View Bookings</h1>
+         <h1 className="display-4 text-center py-3">Bookings</h1>
+         <table class="table">
+            <thead class="thead-dark">
+               <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Time</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+               </tr>
+               <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+               </tr>
+               <tr>
+                  <th scope="row">3</th>
+                  <td>Larry</td>
+                  <td>the Bird</td>
+                  <td>@twitter</td>
+               </tr>
+            </tbody>
+         </table>
       </div>
    );
 }
