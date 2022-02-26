c4b7846aa1d9f0ab1a97ade1bf8bf26fc6917661
akramansari1433 <70878208+akramansari1433@users.noreply.github.com>
2022-02-26 23:36:39 +0530

    admin login added

--- src/components/Admin/Login.js
+++ src/components/Admin/Login.js
@@ -1,31 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import CircularProgress from "@mui/material/CircularProgress";
+import Button from "@mui/material/Button";
+import axios from "axios";
 
 function Login({ setAdmin }) {
-   const handleSumit = () => {
-      window.localStorage.setItem("admin", true);
-      setAdmin(window.localStorage.getItem("admin"));
+   const [username, setUsername] = useState();
+   const [password, setPassword] = useState();
+
+   const [loading, setLoading] = useState(false);
+   const [error, setError] = useState();
+
+   const handleSumit = (e) => {
+      e.preventDefault();
+      setLoading(true);
+      axios
+         .post("/admin/login", { username, password })
+         .then((res) => {
+            if (res.data.message) {
+               setLoading(false);
+               window.localStorage.setItem("admin", true);
+               setAdmin(window.localStorage.getItem("admin"));
+               e.target.reset();
+               setError("");
+            }
+         })
+         .catch((err) => {
+            setError(err.response.data.error);
+            setLoading(false);
+         });
    };
    return (
       <div>
          <h1 className="text-center display-4 py-3">Admin Login</h1>
-         <form className="text-center" onSubmit={handleSumit}>
-            <input
+         <form
+            className="py-3 d-flex flex-column align-items-center"
+            onSubmit={handleSumit}
+         >
+            <TextField
+               label="Username"
+               sx={{ width: "20rem", marginBottom: 3 }}
+               required
                type="text"
-               placeholder="Username"
-               className="mt-3 h4 p-2"
-               required
+               onChange={(e) => setUsername(e.target.value)}
             />
-            <br />
-            <input
+
+            <TextField
+               label="Password"
+               sx={{ width: "20rem", marginBottom: 3 }}
+               required
                type="password"
-               placeholder="Password"
-               className="mt-3 h4 p-2"
-               required
+               onChange={(e) => setPassword(e.target.value)}
             />
-            <br />
-            <button type="submit" className="btn btn-dark mt-3 px-3">
-               Login
-            </button>
+
+            <Button variant="contained" type="submit" color="primary">
+               {loading ? (
+                  <CircularProgress color="inherit" size={25} />
+               ) : (
+                  "Login"
+               )}
+            </Button>
+
+            <p className="text-danger">{error}</p>
          </form>
       </div>
    );
