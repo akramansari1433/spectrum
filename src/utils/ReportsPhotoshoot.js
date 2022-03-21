6544aa7afa703b3e5742288351873fb20a6f1403
akramansari1433 <70878208+akramansari1433@users.noreply.github.com>
2022-03-21 15:46:48 +0530

    some bug fixes

--- src/utils/ReportsPhotoshoot.js
+++ src/utils/ReportsPhotoshoot.js
@@ -7,26 +7,30 @@ export default class ReportPhotoshoot extends Component {
          <div className="mx-3 my-3">
             <h3 className="text-center mb-3">Photoshoots Details</h3>
             <table className="table table-bordered">
-               <tr>
-                  <th scope="col">Date</th>
-                  <th scope="col">Name</th>
-                  <th scope="col">Email</th>
-                  <th scope="col">Phone</th>
-                  <th scope="col">Category</th>
-                  <th scope="col">PaymentId</th>
-                  <th scope="col">Amount Paid</th>
-               </tr>
-               {data.map((p) => (
-                  <tr key={p.adop_id}>
-                     <td>{p.date}</td>
-                     <td>{p.name}</td>
-                     <td>{p.email}</td>
-                     <td>{p.phone}</td>
-                     <td>{p.category}</td>
-                     <td>{p.paymentId}</td>
-                     <td>{p.amount}</td>
+               <thead>
+                  <tr>
+                     <th scope="col">Date</th>
+                     <th scope="col">Name</th>
+                     <th scope="col">Email</th>
+                     <th scope="col">Phone</th>
+                     <th scope="col">Category</th>
+                     <th scope="col">PaymentId</th>
+                     <th scope="col">Amount Paid</th>
                   </tr>
-               ))}
+               </thead>
+               <tbody>
+                  {data.map((p) => (
+                     <tr key={p.photoshootId}>
+                        <td>{p.date}</td>
+                        <td>{p.name}</td>
+                        <td>{p.email}</td>
+                        <td>{p.phone}</td>
+                        <td>{p.category}</td>
+                        <td>{p.paymentId}</td>
+                        <td>{p.amount}</td>
+                     </tr>
+                  ))}
+               </tbody>
             </table>
          </div>
       );
--- src/utils/ReportsStudio.js
+++ src/utils/ReportsStudio.js
@@ -7,24 +7,28 @@ export default class ReportsStudio extends Component {
          <div className="mx-3 my-3">
             <h3 className="text-center mb-3">Studio Booking Details</h3>
             <table className="table table-bordered">
-               <tr>
-                  <th scope="col">Date</th>
-                  <th scope="col">Name</th>
-                  <th scope="col">Email</th>
-                  <th scope="col">Phone</th>
-                  <th scope="col">PaymentId</th>
-                  <th scope="col">Amount Paid</th>
-               </tr>
-               {data.map((p) => (
-                  <tr key={p.adop_id}>
-                     <td>{p.date}</td>
-                     <td>{p.name}</td>
-                     <td>{p.email}</td>
-                     <td>{p.phone}</td>
-                     <td>{p.paymentId}</td>
-                     <td>{p.amount}</td>
+               <thead>
+                  <tr>
+                     <th scope="col">Date</th>
+                     <th scope="col">Name</th>
+                     <th scope="col">Email</th>
+                     <th scope="col">Phone</th>
+                     <th scope="col">PaymentId</th>
+                     <th scope="col">Amount Paid</th>
                   </tr>
-               ))}
+               </thead>
+               <tbody>
+                  {data.map((p) => (
+                     <tr key={p.bookingId}>
+                        <td>{p.date}</td>
+                        <td>{p.name}</td>
+                        <td>{p.email}</td>
+                        <td>{p.phone}</td>
+                        <td>{p.paymentId}</td>
+                        <td>{p.amount}</td>
+                     </tr>
+                  ))}
+               </tbody>
             </table>
          </div>
       );
